a64500b2fe1894fe7a7a41ddc87f3ece6bd0b2cb
HartMason <hartmason85@gmail.com>
2022-07-28 18:31:00 -0500

    added axios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import "./App.css";
-import { Component } from "react";
+import React, { Component } from "react";
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.name)
+    this.setState({                            
+      firstName: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()                           //prevents page reload
+    console.log(this.state.firstName)
+
+    //Where we send data off to a server
+    axios.post('/user', {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+
+    this.setState({
+      firstName: "",
+      lastName: ""
+    })
   }
 
   render() {
-    return <div className="App">
-      <header className="App-header">
-        <p>My 2nd form in React</p>
-        <form>
-          <input type="text"></input>
-          <input type="submit"></input>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>My 2nd form in React</p>
+          <form onSubmit={(e) => {this.handleSubmit(e)}}>
+            <input name="firstName" type="text" value={this.state.firstName} onChange={e => {this.handleChange(e)}}></input>
+            <input name="lastName" type="text" value={this.state.lastName} onChange={e => {this.handleChange(e)}}></input>
+            <input type="submit"></input>
+           
 
-        </form>
-      </header>
-    </div>;
+          </form>
+        </header>
+      </div>
+    );
   }
 }
 export default App;
